8b5ba29d5f3ea4257202f538964bb3be42b5493a
yousef alattal <yousefmohnad984@gmail.com>
2025-01-09 18:21:56 +0300

    Add project files.

---
+++ RestaurantBillCalculator/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace RestaurantBillCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxTableNumber = new System.Windows.Forms.TextBox();
+            this.textBoxWaiterName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.comboBoxBeverage = new System.Windows.Forms.ComboBox();
+            this.comboBoxAppetizer = new System.Windows.Forms.ComboBox();
+            this.comboBoxMainCourse = new System.Windows.Forms.ComboBox();
+            this.comboBoxDessert = new System.Windows.Forms.ComboBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.labelTax = new System.Windows.Forms.Label();
+            this.labelSubtotal = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(112, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Restaurant";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClear.Location = new System.Drawing.Point(129, 469);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(110, 36);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "Clear Button";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Table Number:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(11, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Waiter\'s Name:";
+            // 
+            // textBoxTableNumber
+            // 
+            this.textBoxTableNumber.Location = new System.Drawing.Point(139, 32);
+            this.textBoxTableNumber.Name = "textBoxTableNumber";
+            this.textBoxTableNumber.Size = new System.Drawing.Size(138, 26);
+            this.textBoxTableNumber.TabIndex = 7;
+            // 
+            // textBoxWaiterName
+            // 
+            this.textBoxWaiterName.Location = new System.Drawing.Point(139, 89);
+            this.textBoxWaiterName.Name = "textBoxWaiterName";
+            this.textBoxWaiterName.Size = new System.Drawing.Size(138, 26);
+            this.textBoxWaiterName.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(11, 72);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Appetizer:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(11, 34);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Beverage:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(11, 144);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 16);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Dessert:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(11, 106);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(85, 16);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Main Course:";
+            // 
+            // comboBoxBeverage
+            // 
+            this.comboBoxBeverage.FormattingEnabled = true;
+            this.comboBoxBeverage.Location = new System.Drawing.Point(165, 34);
+            this.comboBoxBeverage.Name = "comboBoxBeverage";
+            this.comboBoxBeverage.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxBeverage.TabIndex = 13;
+            this.comboBoxBeverage.SelectedIndexChanged += new System.EventHandler(this.comboBoxBeverage_SelectedIndexChanged);
+            // 
+            // comboBoxAppetizer
+            // 
+            this.comboBoxAppetizer.FormattingEnabled = true;
+            this.comboBoxAppetizer.Location = new System.Drawing.Point(165, 72);
+            this.comboBoxAppetizer.Name = "comboBoxAppetizer";
+            this.comboBoxAppetizer.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxAppetizer.TabIndex = 14;
+            this.comboBoxAppetizer.SelectedIndexChanged += new System.EventHandler(this.comboBoxAppetizer_SelectedIndexChanged);
+            // 
+            // comboBoxMainCourse
+            // 
+            this.comboBoxMainCourse.FormattingEnabled = true;
+            this.comboBoxMainCourse.Location = new System.Drawing.Point(165, 106);
+            this.comboBoxMainCourse.Name = "comboBoxMainCourse";
+            this.comboBoxMainCourse.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxMainCourse.TabIndex = 15;
+            this.comboBoxMainCourse.SelectedIndexChanged += new System.EventHandler(this.comboBoxMainCourse_SelectedIndexChanged);
+            // 
+            // comboBoxDessert
+            // 
+            this.comboBoxDessert.FormattingEnabled = true;
+            this.comboBoxDessert.Location = new System.Drawing.Point(165, 144);
+            this.comboBoxDessert.Name = "comboBoxDessert";
+            this.comboBoxDessert.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxDessert.TabIndex = 16;
+            this.comboBoxDessert.SelectedIndexChanged += new System.EventHandler(this.comboBoxDessert_SelectedIndexChanged);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(11, 108);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 16);
+            this.label14.TabIndex = 22;
+            this.label14.Text = "Total:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(11, 70);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(33, 16);
+            this.label15.TabIndex = 21;
+            this.label15.Text = "Tax:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(11, 36);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(59, 16);
+            this.label16.TabIndex = 20;
+            this.label16.Text = "Subtotal:";
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotal.Location = new System.Drawing.Point(103, 108);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(38, 16);
+            this.labelTotal.TabIndex = 25;
+            this.labelTotal.Text = "$0.00";
+            // 
+            // labelTax
+            // 
+            this.labelTax.AutoSize = true;
+            this.labelTax.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTax.Location = new System.Drawing.Point(103, 70);
+            this.labelTax.Name = "labelTax";
+            this.labelTax.Size = new System.Drawing.Size(38, 16);
+            this.labelTax.TabIndex = 24;
+            this.labelTax.Text = "$0.00";
+            // 
+            // labelSubtotal
+            // 
+            this.labelSubtotal.AutoSize = true;
+            this.labelSubtotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSubtotal.Location = new System.Drawing.Point(103, 36);
+            this.labelSubtotal.Name = "labelSubtotal";
+            this.labelSubtotal.Size = new System.Drawing.Size(38, 16);
+            this.labelSubtotal.TabIndex = 23;
+            this.labelSubtotal.Text = "$0.00";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBoxTableNumber);
+            this.groupBox1.Controls.Add(this.textBoxWaiterName);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(32, 95);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(303, 144);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Waiter Information";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.comboBoxBeverage);
+            this.groupBox2.Controls.Add(this.comboBoxAppetizer);
+            this.groupBox2.Controls.Add(this.comboBoxMainCourse);
+            this.groupBox2.Controls.Add(this.comboBoxDessert);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(32, 260);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(303, 189);
+            this.groupBox2.TabIndex = 27;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Menu Items";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label16);
+            this.groupBox3.Controls.Add(this.label15);
+            this.groupBox3.Controls.Add(this.label14);
+            this.groupBox3.Controls.Add(this.labelTotal);
+            this.groupBox3.Controls.Add(this.labelSubtotal);
+            this.groupBox3.Controls.Add(this.labelTax);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(32, 524);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(303, 149);
+            this.groupBox3.TabIndex = 28;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Bill Details";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 718);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Restaurant Bill Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxTableNumber;
+        private System.Windows.Forms.TextBox textBoxWaiterName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBoxBeverage;
+        private System.Windows.Forms.ComboBox comboBoxAppetizer;
+        private System.Windows.Forms.ComboBox comboBoxMainCourse;
+        private System.Windows.Forms.ComboBox comboBoxDessert;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Label labelTax;
+        private System.Windows.Forms.Label labelSubtotal;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ RestaurantBillCalculator/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestaurantBillCalculator
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, double> beverages = new Dictionary<string, double>
+        {
+            { "Soda", 1.95 },
+            { "Tea", 1.50 },
+            { "Coffee", 1.25 },
+            { "Mineral Water", 2.95 },
+            { "Juice", 2.50 },
+            { "Milk", 1.50 }
+        };
+
+        private Dictionary<string, double> appetizers = new Dictionary<string, double>
+        {
+            { "Buffalo Wings", 5.95 },
+            { "Potato Skins", 8.95 },
+            { "Nachos", 8.95 },
+            { "Shrimp Cocktail", 12.95 },
+            { "Mushroom Caps", 10.95 },
+            { "Chips and Salsa", 6.95 }
+        };
+
+        private Dictionary<string, double> mainCourses = new Dictionary<string, double>
+        {
+            { "Chicken Alfredo", 13.95 },
+            { "Chicken Piccata", 13.95 },
+            { "Turkey Club", 11.95 },
+            { "Lobster Pie", 19.95 },
+            { "Prime Rib", 20.95 },
+            { "Shrimp Scampi", 18.95 },
+            { "Turkey Dinner", 13.95 },
+            { "Stuffed Chicken", 14.95 },
+            { "Seafood Alfredo", 15.95 }
+        };
+
+        private Dictionary<string, double> desserts = new Dictionary<string, double>
+        {
+            { "Apple Pie", 5.95 },
+            { "Sundae", 3.95 },
+            { "Carrot Cake", 5.95 },
+            { "Mud Pie", 4.95 },
+            { "Apple Crisp", 5.95 }
+        };
+
+        private double subtotal = 0.0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Populate ComboBoxes with Menu Items
+            comboBoxBeverage.Items.AddRange(beverages.Keys.ToArray());
+            comboBoxAppetizer.Items.AddRange(appetizers.Keys.ToArray());
+            comboBoxMainCourse.Items.AddRange(mainCourses.Keys.ToArray());
+            comboBoxDessert.Items.AddRange(desserts.Keys.ToArray());
+        }
+
+        private void comboBoxBeverage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxBeverage.SelectedIndex >= 0)
+            {
+                string selectedBeverage = comboBoxBeverage.SelectedItem.ToString();
+                subtotal += beverages[selectedBeverage];
+                UpdateBill();
+            }
+        }
+
+        private void comboBoxAppetizer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxAppetizer.SelectedIndex >= 0)
+            {
+                string selectedAppetizer = comboBoxAppetizer.SelectedItem.ToString();
+                subtotal += appetizers[selectedAppetizer];
+                UpdateBill();
+            }
+        }
+
+        private void comboBoxMainCourse_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxMainCourse.SelectedIndex >= 0)
+            {
+                string selectedMainCourse = comboBoxMainCourse.SelectedItem.ToString();
+                subtotal += mainCourses[selectedMainCourse];
+                UpdateBill();
+            }
+        }
+
+        private void comboBoxDessert_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxDessert.SelectedIndex >= 0)
+            {
+                string selectedDessert = comboBoxDessert.SelectedItem.ToString();
+                subtotal += desserts[selectedDessert];
+                UpdateBill();
+            }
+        }
+
+        private void UpdateBill()
+        {
+            double tax = subtotal * 0.16; // 16% tax
+            double total = subtotal + tax;
+
+            labelSubtotal.Text = $"Subtotal: ${subtotal:F2}";
+            labelTax.Text = $"Tax: ${tax:F2}";
+            labelTotal.Text = $"Total: ${total:F2}";
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            subtotal = 0.0;
+            labelSubtotal.Text = "Subtotal: $0.00";
+            labelTax.Text = "Tax: $0.00";
+            labelTotal.Text = "Total: $0.00";
+
+            comboBoxBeverage.SelectedIndex = -1;
+            comboBoxAppetizer.SelectedIndex = -1;
+            comboBoxMainCourse.SelectedIndex = -1;
+            comboBoxDessert.SelectedIndex = -1;
+
+            textBoxTableNumber.Text = "";
+            textBoxWaiterName.Text = "";
+        }
+
+        
+    }
+}
